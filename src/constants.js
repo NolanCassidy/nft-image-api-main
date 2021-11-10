353dc39a49b284f15d0b487959f330e4a65d141e
Nolan Cassidy <nolancassidy1@Ggmail.com>
2021-11-10 08:10:20 -0800

    add code

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const HOST = 'https://swapallapk.oss-cn-hongkong.aliyuncs.com/NFT-IMAGE'
+
+module.exports = {
+  HOST
+}
---
+++ src/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "1": {
+    "name": "Glod",
+    "Description": "It is most common available item in SwapAll ecosystem"
+  },
+  "2": {
+    "name": "Silver",
+    "Description": "It is most common available item in SwapAll ecosystem"
+  },
+  "3": {
+    "name": "Bronze",
+    "Description": "It is most common available item in SwapAll ecosystem"
+  }
+}
