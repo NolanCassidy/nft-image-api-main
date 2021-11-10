5848802c87fb19823812d332541ffdcfdcab019d
Nolan Cassidy <nolancassidy1@Ggmail.com>
2021-11-10 08:52:14 -0800

    add supply check

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const moment = require('moment')
 const { HOST } = require('./src/constants')
 const db = require('./src/database')
+const axios = require('axios');
 
 const PORT = process.env.PORT || 5000
 
@@ -20,17 +21,31 @@ app.get('/', function(req, res) {
 
 app.get('/api/token/:token_id', function(req, res) {
   const tokenId = parseInt(req.params.token_id).toString()
-  const nft = db[tokenId]
-  const  Description = nft.Description
-  const data = {
-    'description' : Description,
-    'image': `${HOST}/${tokenId}.png`,
-    'name': nft.name
-  }
-  res.send(data)
+  axios.get('https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0xbad6186E92002E312078b5a1dAfd5ddf63d3f731&apikey=N5UZBNT4F5H5NDNXF7QYSSPUBZ3UIM9HS3')
+  .then(function (response) {
+    // handle success
+    console.log(response.result);
+    if(response.result >= tokenId){
+      const nft = db[tokenId]
+      const  Description = nft.Description
+      const data = {
+        'description' : Description,
+        'image': `${HOST}/${tokenId}.png`,
+        'name': nft.name
+      }
+      res.send(data)
+    }else{
+      next('Stop digging around, you have nothing to find here.');
+    }
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+    next(error);
+  });
+
 })
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
-
