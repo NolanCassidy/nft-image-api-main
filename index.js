0296e5ae09c42b3e106d2941581d33a367bc43e5
Nolan Cassidy <nolancassidy1@Ggmail.com>
2021-11-10 10:35:16 -0800

    add seconds timers

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 
 const PORT = process.env.PORT || 5000
 var supplyCache = 0;
+var lastApiCall = new Date();
 
 const app = express()
   .set('port', PORT)
@@ -33,30 +34,38 @@ app.get('/api/token/:token_id', function(req, res, next) {
     // console.log(tokenId)
     // console.log(supplyCache)
   if(parseInt(supplyCache) < parseInt(tokenId)){
-    axios.get('https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0xbad6186E92002E312078b5a1dAfd5ddf63d3f731&apikey=N5UZBNT4F5H5NDNXF7QYSSPUBZ3UIM9HS3')
-    .then(function (response) {
-      // handle success
-      // console.log('Not Cached')
-      // console.log(response.data.result);
-      supplyCache = response.data.result;
-      if(response.data.result >= tokenId){
-        const nft = db[tokenId]
-        const  Description = nft.Description
-        const data = {
-          'description' : Description,
-          'image': `${HOST}/${tokenId}.png`,
-          'name': nft.name
+    let now = new Date();
+    var seconds = (now.getTime() - lastApiCall.getTime()) / 1000;
+    if(seconds >= 1){
+      // console.log("Seconds: " + seconds);
+      axios.get('https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0xbad6186E92002E312078b5a1dAfd5ddf63d3f731&apikey=N5UZBNT4F5H5NDNXF7QYSSPUBZ3UIM9HS3')
+      .then(function (response) {
+        // handle success
+        // console.log('Not Cached')
+        // console.log(response.data.result);
+        lastApiCall = now;
+        supplyCache = response.data.result;
+        if(response.data.result >= tokenId){
+          const nft = db[tokenId]
+          const  Description = nft.Description
+          const data = {
+            'description' : Description,
+            'image': `${HOST}/${tokenId}.png`,
+            'name': nft.name
+          }
+          res.send(data)
+        }else{
+          res.status(404).send('Stop digging around, you have nothing to find here.');
         }
-        res.send(data)
-      }else{
-        res.status(404).send('Stop digging around, you have nothing to find here.');
-      }
-    })
-    .catch(function (error) {
-      // handle error
-      // console.log(error);
-      res.status(404).send('This contract or token does not exist.');
-    });
+      })
+      .catch(function (error) {
+        // handle error
+        // console.log(error);
+        res.status(404).send('This contract or token does not exist.');
+      });
+    }else{
+      res.status(404).send('Stop digging around, you have nothing to find here!');
+    }
   }else{
     // console.log('Cached');
     const nft = db[tokenId]
